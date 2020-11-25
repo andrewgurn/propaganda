0e616c9e314e40a027d198a195c95a3175f73955
Andrew Weitzel <61878195+andrewgurn@users.noreply.github.com>
2020-11-25 13:49:44 -0500

    Add files via upload

---
+++ scripts/propagandaStartup.sh
@@ -0,0 +1,14 @@
+#Disable DPMS.
+xset -dpms
+xset s off
+xset s noblank
+
+#Clean up stuff
+rm /home/pi/.config/chromium/SingletonLock
+killall -TERM chromium-browser 2>/dev/null;
+sleep 2;
+killall -9 chromium-browser 2>/dev/null;
+
+#Launch propaganda in chromium
+chromium-browser --kiosk --noerrdialogs --disable-translateb --ignore-certificate-errors --app=https://[your webserver address]/propaganda
+
