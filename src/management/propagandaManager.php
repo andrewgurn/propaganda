ae8ce953f2223a305f39870017d177edeaa6d2b6
andrew <weitzel@protonmail.com>
2020-11-25 15:37:51 -0500

    Made it prettier

--- src/management/propagandaManager.php
+++ src/management/propagandaManager.php
@@ -5,15 +5,32 @@
 
 <html>
 	<head>
-		<title>Propaganda Uploader</title>
+		<title>Propaganda Manager</title>
 		<script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
 		<style>
+			body
+			{
+				font-family: Verdana, Geneva, sans-serif;
+				background: #63585E;
+			}
+			
+			.container
+			{
+				background: #8AA29E;
+				color: #FAFAFA;
+				margin-left: auto;
+				margin-right: auto;
+				padding: 20px;
+				text-align: center;
+			}
+			
 			.left
 			{
 				display: inline-block;
 				width: 25%;
 				left: 5%;
 				vertical-align: top;
+				text-align: left;
 			}
 			
 			.right
@@ -25,213 +42,215 @@
 			
 			.imagePreviewDiv img
 			{
-				width: 100%;
+				width: 75%;
 			}
 			
 			.formDiv div
 			{
-				padding: 5px;
+				padding: 10px;
 			}
 			
 		</style>
 	</head>
 	<body>
 		<?php if(in_array($user, UPLOADERS) || is_empty(UPLOADERS)) : ?>
+		<div class='container'>
 		
-		<h1>Propaganda Uploader</h1>
-		<div class="left formDiv">
-			<div>
-				I am adding an: 
-				<select id="contentType">
-					<option value="">...</option>
-					<option value="image">Image</option>
-					<option value="iframe">iFrame</option>
-				</select>
-			</div>
-			<div id="resultDiv">
-			
-			</div>
-			
-			<input type="hidden" id="contentLocation" value="">
-			
-			<div id="image" style="display: none;">
-				Upload your image: 
-				<input type="file" id="uploadedDocument">
-			</div>
-			<div id="iframe" style="display: none;">
-				Enter the web address of your iFrame:
-				<input type="text" id="iframeLocation">
-			</div>
-			<div>
-				Start Date:
-				<input type="date" id="dateStart">
-			</div>
-			<div>
-				End Date:
-				<input type="date" id="dateEnd">
-			</div>
-			<div>
-				Show for:
-				<input type="text" id="displayTime" length="3"> seconds
-			</div>
-			<div>
-				<input type="button" id="submit" value="Sumbit!">
-			</div>
-			<div id="addResultDiv">
-			
-			</div>
-			<?php
-				echo("<input type='hidden' id='addedBy' value='$user'>");
-			?>
-		</div>
-		<div class="right">
-			<div class="imagePreviewDiv">
-				<img id="previewImage" src="../images/preview.jpg">
-			</div>
-		</div>
-		<div>
-			<h1>Here's everything currently active:</h1>
-			<div id="allContent">
+			<h1>Propaganda Uploader</h1>
+			<div class="left formDiv">
+				<div>
+					I am adding an: 
+					<select id="contentType">
+						<option value="">...</option>
+						<option value="image">Image</option>
+						<option value="iframe">iFrame</option>
+					</select>
+				</div>
+				<div id="resultDiv">
 				
-			</div>
-		</div>
-		<script>
-			$(document).ready(function(){
-				allActiveContent();
-			});
-			
-			//show the appropriate fields based on the content type
-			$('#contentType').on('change', function(){
+				</div>
 				
-				if($('#contentType').val() == 'image')
-				{
-					$('#iframe').fadeOut();
-					$('#image').delay(500).fadeIn();
-				}
-				else if($('#contentType').val() == 'iframe')
-				{
-					$('#image').fadeOut();
-					$('#iframe').delay(500).fadeIn();
-				}
-			});
-			
-			//after the user selects an image, this will upload it to the propaganda/propagandaUploads directory
-			$("#uploadedDocument").on("change", function(){
+				<input type="hidden" id="contentLocation" value="">
+				
+				<div id="image" style="display: none;">
+					Upload your image: 
+					<input type="file" id="uploadedDocument">
+				</div>
+				<div id="iframe" style="display: none;">
+					Enter the web address of your iFrame:
+					<input type="text" id="iframeLocation">
+				</div>
+				<div>
+					Start Date:
+					<input type="date" id="dateStart">
+				</div>
+				<div>
+					End Date:
+					<input type="date" id="dateEnd">
+				</div>
+				<div>
+					Show for:
+					<input type="text" id="displayTime" style="width: 10%;"> seconds
+				</div>
+				<div>
+					<input type="button" id="submit" value="Sumbit!">
+				</div>
+				<div id="addResultDiv">
+				
+				</div>
+				<?php
+					echo("<input type='hidden' id='addedBy' value='$user'>");
+				?>
+			</div>
+			<div class="right">
+				<div class="imagePreviewDiv">
+					<img id="previewImage" src="../images/preview.jpg">
+				</div>
+			</div>
+			<div>
+				<h1>Here's everything currently active:</h1>
+				<div id="allContent">
+					
+				</div>
+			</div>
+			<script>
+				$(document).ready(function(){
+					allActiveContent();
+				});
+				
+				//show the appropriate fields based on the content type
+				$('#contentType').on('change', function(){
+					
+					if($('#contentType').val() == 'image')
+					{
+						$('#iframe').fadeOut();
+						$('#image').delay(500).fadeIn();
+					}
+					else if($('#contentType').val() == 'iframe')
+					{
+						$('#image').fadeOut();
+						$('#iframe').delay(500).fadeIn();
+					}
+				});
+				
+				//after the user selects an image, this will upload it to the propaganda/propagandaUploads directory
+				$("#uploadedDocument").on("change", function(){
 
-				var formDataObject = new FormData();
-				var files = $('#uploadedDocument')[0].files;
+					var formDataObject = new FormData();
+					var files = $('#uploadedDocument')[0].files;
 
-				if(files.length > 0 )
-				{
-					formDataObject.append('file',files[0]);
+					if(files.length > 0 )
+					{
+						formDataObject.append('file',files[0]);
 
+						$.ajax({
+							url: 'propagandaUpload.php',
+							type: 'post',
+							data: formDataObject,
+							contentType: false,
+							processData: false,
+							success: function(response)
+							{
+								if(response != 0)
+								{
+									$("#previewImage").attr('src', '../'+response); 
+									$("#contentLocation").val(response); 
+									
+								}
+								else
+								{
+									$("#resultDiv").html("Well, your file didn't upload.  It has to be a JPG, PNG, or GIF and it must be less than 8MB in size."); 
+								}
+							},
+						});
+					}
+					else
+					{
+						$("#resultDiv").html("You never selected a file, so...");
+					}
+				});
+				
+				$("#iframeLocation").on("change", function(){
+					$('#contentLocation').val($('#iframeLocation').val());
+				});
+				
+				//insert everything into the db	
+				$("#submit").on("click", function(){
+					
+					var contentType = $('#contentType').val();
+					var contentLocation = $('#contentLocation').val();
+					var dateStart = $('#dateStart').val();
+					var dateEnd = $('#dateEnd').val();
+					var displayTime = $('#displayTime').val();
+					var addedBy = $('#addedBy').val();
+					
 					$.ajax({
-						url: 'propagandaUpload.php',
+					    dataType: 'text',
+					    type: 'POST',
+					    url: 'propagandaAdd.php',
+					    data: { 'contentType' : contentType , 'contentLocation' : contentLocation , 'dateStart' : dateStart , 'dateEnd' : dateEnd , 'displayTime' : displayTime , 'addedBy' : addedBy },
+					    contentType: "application/x-www-form-urlencoded;",
+					    success: function(result){
+						  	
+						   $('#addResultDiv').html(result);  
+						   $('#contentType').val("");
+						   $('#contentLocation').val("")
+						   $('#dateEnd').val("");	
+						   $('#displayTime').val("");
+						   $("#previewImage").attr('src', '../images/preview.jpg'); 
+						   allActiveContent();
+					    },
+					    error: function(xhr, textStatus, errorThrown) {
+					       	
+						   $('#addResultDiv').html('<div class="error">AJAX error submitting your content!  Weird!  Did you lose your network connection?</div>');
+						  
+					    }
+					});
+					
+				});
+				
+				function allActiveContent()
+				{
+					$.ajax({
+						url: 'propagandaGetActiveContent.php',
 						type: 'post',
-						data: formDataObject,
 						contentType: false,
 						processData: false,
 						success: function(response)
 						{
-							if(response != 0)
-							{
-								$("#previewImage").attr('src', '../'+response); 
-								$("#contentLocation").val(response); 
-								
-							}
-							else
-							{
-								$("#resultDiv").html("Well, your file didn't upload.  It has to be a JPG, PNG, or GIF and it must be less than 8MB in size."); 
-							}
+							$("#allContent").html(response); 	
 						},
 					});
 				}
-				else
+				
+				function propagandaDeleteContent(contentID)
 				{
-					$("#resultDiv").html("You never selected a file, so...");
+					
+					$.ajax({
+						url: 'propagandaDeleteContent.php',
+						type: 'post',
+						data: { 'contentID' : contentID },
+						contentType: "application/x-www-form-urlencoded;",
+						success: function(response)
+						{
+							allActiveContent();
+						},
+					});
+					
 				}
-			});
-			
-			$("#iframeLocation").on("change", function(){
-				$('#contentLocation').val($('#iframeLocation').val());
-			});
-			
-			//insert everything into the db	
-			$("#submit").on("click", function(){
 				
-				var contentType = $('#contentType').val();
-				var contentLocation = $('#contentLocation').val();
-				var dateStart = $('#dateStart').val();
-				var dateEnd = $('#dateEnd').val();
-				var displayTime = $('#displayTime').val();
-				var addedBy = $('#addedBy').val();
 				
-				$.ajax({
-				    dataType: 'text',
-				    type: 'POST',
-				    url: 'propagandaAdd.php',
-				    data: { 'contentType' : contentType , 'contentLocation' : contentLocation , 'dateStart' : dateStart , 'dateEnd' : dateEnd , 'displayTime' : displayTime , 'addedBy' : addedBy },
-				    contentType: "application/x-www-form-urlencoded;",
-				    success: function(result){
-					  	
-					   $('#addResultDiv').html(result);  
-					   $('#contentType').val("");
-					   $('#contentLocation').val("")
-					   $('#dateEnd').val("");	
-					   $('#displayTime').val("");
-					   $("#previewImage").attr('src', '../images/preview.jpg'); 
-					   allActiveContent();
-				    },
-				    error: function(xhr, textStatus, errorThrown) {
-				       	
-					   $('#addResultDiv').html('<div class="error">AJAX error submitting your content!  Weird!  Did you lose your network connection?</div>');
-					  
-				    }
-				});
-				
-			});
-			
-			function allActiveContent()
-			{
-				$.ajax({
-					url: 'propagandaGetActiveContent.php',
-					type: 'post',
-					contentType: false,
-					processData: false,
-					success: function(response)
-					{
-						$("#allContent").html(response); 	
-					},
-				});
-			}
-			
-			function propagandaDeleteContent(contentID)
-			{
-				
-				$.ajax({
-					url: 'propagandaDeleteContent.php',
-					type: 'post',
-					data: { 'contentID' : contentID },
-					contentType: "application/x-www-form-urlencoded;",
-					success: function(response)
-					{
-						allActiveContent();
-					},
-				});
-				
-			}
+			</script>
 			
 			
-		</script>
+			<?php else : ?>
 		
-		
-		<?php else : ?>
-	
-				<!-- Bad Login -->
-				<p>
-					Hey, so, you're not allowed to use this page.  Sorry!
-				</p>
-		<?php endif; ?>
+					<!-- Bad Login -->
+					<p>
+						Hey, so, you're not allowed to use this page.  Sorry!
+					</p>
+			<?php endif; ?>
+		</div>	
 	</body>
 
 	
